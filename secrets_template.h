93e03e7015ab84544d408e4920db495a8a924460
Daniel Belchior <danielbelchior@gmail.com>
2025-09-04 23:24:50 +0200

    Update secrets_template.h

--- secrets_template.h
+++ secrets_template.h
@@ -2,18 +2,18 @@
 #pragma once
 
 // CONFIG WIFI
-#define WIFI_SSID "..."
-#define WIFI_PASSWORD "..."
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
 
 // CONFIG HOME ASSISTANT
 #define HASS_HOST "homeassistant.local"
 #define HASS_PORT 8123
-#define HASS_TOKEN "..."
+#define HASS_TOKEN "TOKEN"
 
 // SENSORS
-#define SENSOR_1_ENTITY_ID "sensor.XXX"
-#define SENSOR_2_ENTITY_ID "sensor.XXX"
+#define SENSOR_1_ENTITY_ID "sensor.sensor_1"
+#define SENSOR_2_ENTITY_ID "sensor.sensor_2"
 
-// Log UDP endpoint
-#define LOG_UDP_IP "UDP SERVER IP"
+// Log UDP endpoint (for compatibility with logging.cpp)
+#define LOG_UDP_IP "logger.local"
 #define LOG_UDP_PORT 12201
